70f9e89f27042bb69bf2025cf74d9f28239d479c
Anurag1020 <anurag.mendhe14@gmail.com>
2022-07-29 16:34:08 +0530

    EmpPrAb

---
+++ EmpWageProgram.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee_Wage_Computation_Problem
+{
+    public static class EmpWageProgram
+    {
+        public static void EmpWage()
+        {
+            Random random = new Random();
+            int Empis = random.Next(2);
+
+            if (Empis == 0)
+            {
+                Console.WriteLine("Emploee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Emploee is Abcent");
+            }
+        }
+    }
+}
