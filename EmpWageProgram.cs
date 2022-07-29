88ed9dca8f6747bea553ac3fefe51f4b59d343d7
Anurag1020 <anurag.mendhe14@gmail.com>
2022-07-29 16:40:04 +0530

    EmpWage

--- EmpWageProgram.cs
+++ EmpWageProgram.cs
@@ -10,12 +10,16 @@ namespace Employee_Wage_Computation_Problem
     {
         public static void EmpWage()
         {
+            int WagePrHr = 20;
+            int FullDayHr = 8;
+
             Random random = new Random();
             int Empis = random.Next(2);
 
             if (Empis == 0)
             {
                 Console.WriteLine("Emploee is Present");
+                Console.WriteLine(WagePrHr * FullDayHr);
             }
             else
             {
