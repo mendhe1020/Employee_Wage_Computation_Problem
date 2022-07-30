9925c0dd8ca4000966ca98f352ff8318af220106
Anurag1020 <anurag.mendhe14@gmail.com>
2022-07-30 07:01:26 +0530

    Switch

--- EmpWageProgram.cs
+++ EmpWageProgram.cs
@@ -8,29 +8,45 @@ namespace Employee_Wage_Computation_Problem
 {
     public static class EmpWageProgram
     {
+        const int WagePrHr = 20;
+        const int FullDayHr = 8;
+        const int PartTimeHr = 4;
+        const int NumOfWorkingDays = 20;
         public static void EmpWage()
         {
-            int WagePrHr = 20;
-            int FullDayHr = 8;
-            int PartTimeHr = 4;
+            int TotalEmpWage = 0;
+            int EmpWage = 0;
 
-            Random random = new Random();
-            int Empis = random.Next(3);
+            for(int day =0;day<NumOfWorkingDays;day++)
+            {
+                Random random = new Random();
+                int Empis = random.Next(3);
 
-            if (Empis == 0)
-            {
-                Console.WriteLine("Emploee is Present");
-                Console.WriteLine(WagePrHr * FullDayHr);
+                switch(Empis)
+                {
+                    case 1 :
+                        Console.WriteLine(WagePrHr * FullDayHr);
+                        break;
+
+                    case 2 :
+                        Console.WriteLine(WagePrHr * PartTimeHr);
+                        break;
+
+                    default:
+                        Console.WriteLine("Emp is Abcent");
+                        break;
+
+                }
+
             }
-            else if (Empis == 1)
-            {
-                Console.WriteLine("Emp is part time present");
-                Console.WriteLine(WagePrHr * PartTimeHr);
-            }
-            else
-            {
-                Console.WriteLine("Emploee is Abcent");
-            }
+            EmpWage = WagePrHr * PartTimeHr;
+            TotalEmpWage += EmpWage;
+            Console.WriteLine(TotalEmpWage);
+            
+
+
+
+
         }
     }
 }
